4f506b4eb30e5eb763df13e3dc98d3aae30a90e6
Elaine Jeffery <duwaarsteamjeffery@gmail.com>
2021-12-30 12:35:00 -0500

    Solver is working. Main i/o is from/to files.

--- word_finder.py
+++ word_finder.py
@@ -1,7 +1,30 @@
 
 
 def main():
-    assert(False), 'No main function implemented.'
+    with open('words_alpha.txt', 'r') as file:
+        word_list = file.read().split()
+
+    with open('letters.txt', 'r') as file:
+        letter_list = file.read().split()
+
+    with open('solution_words.txt', 'w') as solutions_file:
+        solution_count = 0
+        for word in word_list:
+            if not (letter_list[0] in word):
+                continue
+            elif (len(word) < 4):
+                continue
+
+            for i, letter in enumerate(word):
+                if not (letter in letter_list):
+                    break
+                if (i == len(word) - 1):
+                    solutions_file.write(word + '\n')
+                    solution_count += 1
+
+        print(str(solution_count) + " words found in word list.")
+
 
 if __name__ == '__main__':
     main()
+
