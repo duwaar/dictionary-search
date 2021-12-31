0d16311d4bbfd43ecb58b2b8d0a3868ab88cfc2e
Elaine Jeffery <duwaarsteamjeffery@gmail.com>
2021-12-31 11:45:02 -0500

    Renamed project to "dictionary search"

--- dictionary_search.py
+++ dictionary_search.py
@@ -1,5 +1,8 @@
+import wx
+import dictionary_search_gui as gui
 
 
+'''
 def main():
     with open('words_alpha.txt', 'r') as file:
         word_list = file.read().split()
@@ -22,6 +25,25 @@ def main():
 
     print(str(len(solution_words)) + " words found in word list:")
     print(solution_words)
+'''
+
+class MyFrame(gui.SearchFrame):
+    def __init__(self, *args, **kwds):
+        gui.SearchFrame.__init__(self, *args, **kwds)
+
+    def on_search_button_pressed(self):
+        print('No function implemented for "on_search_button_pressed"')
+
+class DictionarySearchApp(wx.App):
+    def OnInit(self):
+        self.frame = MyFrame(None, wx.ID_ANY, "")
+        self.SetTopWindow(self.frame)
+        self.frame.Show()
+        return True
+
+def main():
+    app = DictionarySearchApp(0)
+    app.MainLoop()
 
 
 if __name__ == '__main__':
